37620d8fc7233ebd2661a366927a8eb67c5679be
Rhodexa <69089201+Rhodexa@users.noreply.github.com>
2022-04-19 19:12:46 -0300

    Create hardware_definitions.h

---
+++ bambu_core/hardware_definitions.h
@@ -0,0 +1,9 @@
+#ifndef HW_DEF_H
+#defin HW_DEF_H
+// This is just a placeholder, but there should be all hardware mapping if necessary.
+
+#define SPK_OUT 0
+#define STATUS_LED 0
+#define AUX_OUT 0
+
+#endif
